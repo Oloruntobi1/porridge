376b1b986eb4249f8dda786ddc0a449c99d30eb8
Askari <blgntobi@gmail.com>
2019-11-25 20:19:30 +0100

    test

--- index.php
+++ index.php
@@ -7,8 +7,11 @@ $dbconn = pg_connect("host=ec2-54-243-47-196.compute-1.amazonaws.com port=5432 d
     or die('Could not connect: ' . pg_last_error());
 
 // Performing SQL query
-$query = 'CREATE TABLE test';
+$query = 'SELECT * FROM animasahun';
 $result = pg_query($dbconn, $query) or die('Query failed: ' . pg_last_error());
+
+
+
 
 
 
