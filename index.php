9e0088525624dedecff8106903bfe719bead275d
Askari <blgntobi@gmail.com>
2019-11-25 20:25:28 +0100

    fresh

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once('index.html');
 
 
 $dbconn = pg_connect("host=ec2-54-243-47-196.compute-1.amazonaws.com port=5432 dbname=dd1toaipcsjn9j user=pxpdlmcfxgzeyi password=6914a3d3b77aa6b46b4cd348a56a4f1a13ed2c85f06f768bfed359c9b9a931d5")
